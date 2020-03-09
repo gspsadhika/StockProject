3463b33fac6180e39e533e69439c10966fd8e9d6
gspsadhika <gspsadhika@gmail.com>
2020-03-09 18:03:00 +0530

    project

--- Stock/StockMarket/src/app/login/login.component.ts
+++ Stock/StockMarket/src/app/login/login.component.ts
@@ -21,57 +21,61 @@ export class LoginComponent implements OnInit {
 
     this.loginForm= this.formBuilder.group({
       username: ['',  Validators.required],
-      password:['', Validators.required],
-             
+      password:['', Validators.required]        
 
     });
   
-      this.userService.getAllUsers().subscribe(u => {
-        this.users=u;
-      })
+      // this.userService.getAllUsers().subscribe(u => {
+      //   this.users=u;
+      // })
   }
   submit()
   {
     console.log(this.loginForm.value);
   }
 
-  isValid()
-  {
-    let admin_userName="admin";
-    let admin_password="admin";
-    let userName = this.loginForm.controls.username.value;
-    let password = this.loginForm.controls.password.value;
+//   isValid()
+//   {
+//     let admin_userName="admin";
+//     let admin_password="admin";
+//     let userName = this.loginForm.controls.username.value;
+//     let password = this.loginForm.controls.password.value;
 
-    if((userName== admin_userName) && (password== admin_password)){
-      this.router.navigate(['/admin'])
-    }
-    else{
-      if(this.login(userName, password)){
-        if(this.userService.isActivated(this.currentUser)){
-          localStorage.removeItem('userId');
-          localStorage.setItem('userId', this.currentUser.id.toString());
-          this.router.navigate(['/userLand']);             
-        }
-        else{
-          alert("please activate your account to login");
-        }
+//     if((userName== admin_userName) && (password== admin_password)){
+//       this.router.navigate(['/admin'])
+//     }
+//     else{
+//       if(this.login(userName, password)){
+//         if(this.userService.isActivated(this.currentUser)){
+//           localStorage.removeItem('userId');
+//           localStorage.setItem('userId', this.currentUser.id.toString());
+//           this.router.navigate(['/userLand']);             
+//         }
+//         else{
+//           alert("please activate your account to login");
+//         }
 
-      }
-      else{
-        alert("invalid username or password");
-        this.loginForm.reset();
-      }
-    }
-  }
-  login(userName:string, password:string) {
-    for(let user of this.users){
-      if((userName == user.username) &&(password == user.password))
-      this.currentUser=user;
-      return true;
-    }
+//       }
+//       else{
+//         alert("invalid username or password");
+//         this.loginForm.reset();
+//       }
+//     }
+//   }
+//   login(userName:string, password:string) {
+//     for(let user of this.users){
+//       if((userName == user.username) &&(password == user.password))
+//       this.currentUser=user;
+//       return true;
+//     }
   
-  return false;
+//   return false;
+// }
+// login()
+// {
+//   let username= this.loginForm.get("username").
+// }
+
 }
 
 
-}
